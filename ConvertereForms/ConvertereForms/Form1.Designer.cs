7e51f5d69c0b67b1f205b201923057c62b4d28f3
Robra1_cp <SND\Robra1_cp>
2015-08-21 01:46:36 +0000

    Hallo

---
+++ ConvertereForms/ConvertereForms/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ConvertereForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(331, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 27);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Pfad...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(14, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(311, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(14, 42);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(55, 19);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Word";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(87, 42);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(56, 19);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "Excel";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(14, 67);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(73, 19);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Text = "Löschen";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 43);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(14, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(404, 253);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 357);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox textBox2;
+    }
+}
+
---
+++ ConvertereForms/ConvertereForms/Form1.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace ConvertereForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //Einfügen des aktuellen Datum zu Beginn des Logs
+            textBox2.Text += DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString() + "\n";
+            textBox2.Text += "---------------------------------------------------------------------------\n";
+        }
+        #region Deklarationen
+        //String für den Pfad
+        string srcPfad = "";
+        //Listen in denen die Pfade der Dokumente absgespeichert werden
+        List<string> dateienWord = new List<string>();
+        List<string> dateienExcel = new List<string>();
+        //Count, Anzahl der konvertieren Dokumente 
+        int count = 0;
+        #endregion
+
+
+        /// <summary>
+        /// Erzwingt ein updaten der Textbox (nicht schön, aber funktioniert)
+        /// </summary>
+        private void upText()
+        {
+            textBox2.Invalidate();
+            textBox2.Update();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Eingabe des Pfades
+            folderBrowserDialog1.ShowDialog();
+            srcPfad = folderBrowserDialog1.SelectedPath;
+            textBox1.Text = srcPfad;
+
+            //abspeichern alles Dateien
+            string[] FileNames = Directory.GetFiles(srcPfad, "*.*", SearchOption.AllDirectories);
+
+            //geht alle Dateien durch
+            foreach (string s in FileNames)
+            {
+                //Wählt die Dateiendung
+                switch (Path.GetExtension(s))
+                {
+                    case ".doc":
+                        dateienWord.Add(s);
+                        textBox2.Text += s + " gefunden \n";
+                        upText();
+                        break;
+                    case ".xls":
+                        dateienExcel.Add(s);
+                        textBox2.Text += s + " gefunden \n";
+                        upText();
+                        break;
+ 
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //Word
+            if (checkBox1.Checked)
+            {
+                convertWordFiles();
+                //Remove
+                if (checkBox3.Checked)
+                {
+                    foreach (string s in dateienWord)
+                    {
+                        File.Delete(s);
+                        textBox2.Text += s + " gelöscht\n";
+                        upText();
+                    }
+                }
+            }
+            //Excel
+            if (checkBox2.Checked)
+            {
+                convertExcelFiles();
+                //Remove
+                if (checkBox3.Checked)
+                {
+                    foreach (string s in dateienExcel)
+                    {
+                        File.Delete(s);
+                        textBox2.Text += s + " gelöscht\n";
+                        upText();
+                    }
+                }
+            }
+            //Abspeichern der Log-Datei
+            textBox2.Text += count + " Datei(en) erfolgreich konvertiert";
+            File.WriteAllLines(srcPfad + @"\log.txt", textBox2.Lines);
+        }
+
+        /// <summary>
+        /// Konvertiert alle Dateien in dateienWord
+        /// </summary>
+        private void convertWordFiles()
+        {
+            //Erzeugt eine neue Word-Anwendung
+            Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+            foreach (string s in dateienWord)
+            {
+                //Einlesen des Dokumentes
+                var sourceFile = new FileInfo(s);
+                var doc = word.Documents.Open(sourceFile.FullName);
+                string newFile = "";
+
+                //Prüfen auf Makro
+                if (doc.HasVBProject)
+                {
+                    //Abspeichern des Dokumentes
+                    newFile = sourceFile.FullName.Replace(".doc", ".docm");
+                    doc.SaveAs2(FileName: newFile, FileFormat: WdSaveFormat.wdFormatXMLDocumentMacroEnabled, CompatibilityMode: Microsoft.Office.Interop.Word.WdCompatibilityMode.wdWord2003);
+
+                }
+                else
+                {
+                    //Abspeichern des Dokumentes
+                    newFile = sourceFile.FullName.Replace(".doc", ".docx");
+                    doc.SaveAs2(FileName: newFile, FileFormat: WdSaveFormat.wdFormatXMLDocument, CompatibilityMode: Microsoft.Office.Interop.Word.WdCompatibilityMode.wdWord2003);
+                }
+                
+                //schließt das Dokument
+                word.ActiveDocument.Close();
+
+                //Zeit anpassen
+                File.SetCreationTime(newFile, File.GetCreationTime(s));
+                File.SetLastWriteTime(newFile, File.GetLastWriteTime(s));
+                File.SetLastAccessTime(newFile, File.GetLastAccessTime(s));
+
+                //Log
+                textBox2.Text += s + " konvertiert\n";
+                upText();
+                count++;
+            }
+            //Beendet Word
+            word.Quit();
+        }
+
+        /// <summary>
+        /// Konvertiert alle Dokumente in dateinExcel
+        /// </summary>
+        private void convertExcelFiles()
+        {
+            //Erzeugt eine neue Excel-Anwendung
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+
+            foreach (string s in dateienExcel)
+            {
+                //Einlesen des Dokumentes
+                var sourceFile = new FileInfo(s);
+                var doc = excel.Workbooks.Open(sourceFile.FullName);
+
+                string newFile = "";
+
+                //Prüfen auf Makro
+                if (doc.HasVBProject)
+                {
+                    //Abspeichern des Dokumentes
+                    newFile = sourceFile.FullName.Replace(".xls", ".xlsm");
+                    doc.SaveAs(Filename: newFile, FileFormat: Microsoft.Office.Interop.Excel.XlFileFormat.xlOpenXMLWorkbookMacroEnabled);
+
+                }
+                else
+                {
+                    //Abspeichern des Dokumentes
+                    newFile = sourceFile.FullName.Replace(".xls", ".xlsx");
+                    doc.SaveAs(Filename: newFile, FileFormat: Microsoft.Office.Interop.Excel.XlFileFormat.xlOpenXMLWorkbook);
+                }
+                //schließt das Dokument
+                excel.ActiveWorkbook.Close();
+
+                //Zeit anpassen
+                File.SetCreationTime(newFile, File.GetCreationTime(s));
+                File.SetLastWriteTime(newFile, File.GetLastWriteTime(s));
+                File.SetLastAccessTime(newFile, File.GetLastAccessTime(s));
+
+                //Log
+                textBox2.Text += s + " konvertiert\n";
+                upText();
+                count++;
+            }
+            //Beendet Excel
+            excel.Quit();
+        }
+    }
+}
